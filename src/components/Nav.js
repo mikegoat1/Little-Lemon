4bbddffcbe665ea69decba1fe9cbe65782351cd7
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-05-01 17:16:24 -0700

    adding the proper syntax for links in Navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,33 @@ import React from "react";
 function Nav() {
     return (
         <nav>
-
+            <img src="../icons_assets/Logo .svg"></img>
+            <ul>
+                <li>
+                    {/* This will possibly be on the same page */}
+                    <a href="#home">HOME</a>
+                </li>
+                <li>
+                    {/* This will possibly be on the same page */}
+                    <a href="#about">ABOUT</a>
+                </li>
+                <li>
+                    {/* This will be on a new generated page */}
+                    <a to="/menu">MENU</a>
+                </li>
+                <li>
+                    {/* This will be on a new generated page */}
+                    <a to="/reservations">RESERVATIONS</a>
+                </li>
+                <li>
+                    {/* This will be on a new gnerated page */}
+                    <a to="/order_online">ORDER ONLINE</a>
+                </li>
+                <li>
+                    {/* This will be on a new generated page */}
+                    <a to="/login">LOGIN</a>
+                </li>
+            </ul>
         </nav>
     )
 }
