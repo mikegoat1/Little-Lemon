dac16296f3d4a72fe8e35583a927562f7fa262de
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-05-02 14:29:20 -0700

    adding all changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Header from "./components/Header.js";
 import Main from "./components/Main.js";
 import Footer from "./components/Footer.js"
+import Nav from './components/Nav.js';
 
 function App() {
   return (
     <>
       <Header>
-
+        <Nav />
       </Header>
 
       <Main>
