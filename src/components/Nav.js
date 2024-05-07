be354ae10be71b612a1f83006e33edee77c00ca7
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-05-07 12:38:47 -0700

    adding the basics of the Navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,40 +1,54 @@
 import React from "react";
-import { HStack } from "@chakra-ui/react";
+import { Image, Box } from "@chakra-ui/react";
 import headerLogo from "../icons_assets/Logo.svg"
 
 const Nav = () => {
     return (
-        <HStack>
-            <nav>
-                <img src={headerLogo} alt="Little Lemon logo"></img>
-                <ul>
-                    <li>
-                        {/* This will possibly be on the same page */}
-                        <a href="#home">HOME</a>
-                    </li>
-                    <li>
-                        {/* This will possibly be on the same page */}
-                        <a href="#about">ABOUT</a>
-                    </li>
-                    <li>
-                        {/* This will be on a new generated page */}
-                        <a to="/menu" href="#menu">MENU</a>
-                    </li>
-                    <li>
-                        {/* This will be on a new generated page */}
-                        <a to="/reservations" href="#reservations">RESERVATIONS</a>
-                    </li>
-                    <li>
-                        {/* This will be on a new gnerated page */}
-                        <a to="/order_online" href="#order_online">ORDER ONLINE</a>
-                    </li>
-                    <li>
-                        {/* This will be on a new generated page */}
-                        <a to="/login" href="#login">LOGIN</a>
-                    </li>
-                </ul>
-            </nav>
-        </HStack>
+        <>
+            <Box maxWidth="1280px" margin="0 auto">
+
+                <Box
+                    display="flex"
+                    justifyContent="space-between"
+                    px={32}
+                    py={16}
+                    alignItems="center"
+                    fontFamily="Karla"
+                    fontSize={20}
+                >
+                    <nav>
+                        <Image src={headerLogo} alt="Little Lemon logo"></Image>
+                    </nav>
+                    <ul style={{ listStyleType: 'none', padding: 0, margin: 0, display: 'flex' }}>
+                        <li style={{ marginRight: "10px" }}>
+                            {/* This will possibly be on the same page */}
+                            <a href="#home" style={{ textDecoration: "none", color:"black" }}>HOME</a>
+                        </li>
+                        <li style={{ marginRight: "10px" }}>
+                            {/* This will possibly be on the same page */}
+                            <a href="#about" style={{ textDecoration: "none", color:"black" }}>ABOUT</a>
+                        </li>
+                        <li style={{ marginRight: "10px" }}>
+                            {/* This will be on a new generated page */}
+                            <a to="/menu" href="#menu" style={{ textDecoration: "none", color:"black" }}>MENU</a>
+                        </li>
+                        <li style={{ marginRight: "10px" }}>
+                            {/* This will be on a new generated page */}
+                            <a to="/reservations" href="#reservations" style={{ textDecoration: "none", color:"black" }}>RESERVATIONS</a>
+                        </li>
+                        <li style={{ marginRight: "10px" }}>
+                            {/* This will be on a new gnerated page */}
+                            <a to="/order_online" href="#order_online" style={{ textDecoration: "none", color:"black" }}>ORDER ONLINE</a>
+                        </li>
+                        <li style={{ marginRight: "10px" }}>
+                            {/* This will be on a new generated page */}
+                            <a to="/login" href="#login" style={{ textDecoration: "none", color:"black" }}>LOGIN</a>
+                        </li>
+                    </ul>
+
+                </Box>
+            </Box>
+        </>
     )
 }
 
