8bff10551186c44f253b85c29a22ab64f3c30d11
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-05-14 15:49:04 -0700

    adding card component

---
+++ src/components/Card.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { VStack, Image, Box, Text, HStack, Icon } from "@chakra-ui/react";
+
+
+const Card = () => {
+    return (
+        <Box>
+            <VStack>
+                <Box>
+                    <Image>
+                        Random Image for Dishes
+                    </Image>
+                </Box>
+                <Box>
+                    <HStack>
+                        {/* Title and prices */}
+                    </HStack>
+                </Box>
+                <Box>
+                    <Text>
+                        {/* Description */}
+                    </Text>
+                </Box>
+                <Box>
+                    <HStack>
+                        {/* Order Delivery  and Icon*/}
+                        <Text>
+
+                        </Text>
+                        <Icon>
+
+                        </Icon>
+                    </HStack>
+                </Box>
+            </VStack>
+        </Box>
+    )
+}
+
+export default Card;
