6f6213f1a42717b451690aac53bdbd336f42585c
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-05-21 13:31:19 -0700

    structuring the Testimonials section

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { VStack, HStack, Heading, Text, Image } from "@chakra-ui/react";
+import { VStack, HStack, Heading, Text, Image, Box } from "@chakra-ui/react";
 
 const Testimonials = () => {
     return (
         <>
-
+            <VStack>
+                <Box>
+                    <Heading>Testimonials</Heading>
+                </Box>
+                <HStack>
+                    
+                </HStack>
+            </VStack>
         </>
     )
 }
 
-export default Testimonials; 
+export default Testimonials;
