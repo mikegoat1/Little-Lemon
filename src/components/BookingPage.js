ac8556afaa3b2c60f8980e0c9785e24a7b8a9188
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-05-23 11:41:07 -0700

    adding both components skelington for app

---
+++ src/components/BookingPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useFormik } from "formik";
+import {
+    Box,
+    Button,
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Heading,
+    Input,
+    Select,
+    Textarea,
+    VStack,
+    HStack
+} from "@chakra-ui/react";
