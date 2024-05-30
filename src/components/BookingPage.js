de7ef238557ded5f53d9c8ffa30fff438aa2988b
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-05-29 19:57:12 -0700

    adjusting to be in line with project rules

--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useReducer } from "react";
+import FormSubmit from "./BookingForm";
 
-const BookingPage = ({ children }) => {
+const initializeTimes = () => {
+    return ["12:00 PM", "1:00 PM", "2:00 PM"];
+};
+
+const updateTimes = (state, action) => {
+    switch (action.type) {
+        case "UPDATE_TIMES":
+            return ["12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM"];
+        default:
+            return state;
+    }
+};
+
+const BookingPage = () => {
+    const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
     return (
         <>
-            {children}
+            <FormSubmit availableTimes={availableTimes} dispatch={dispatch} />
         </>
     );
 };
