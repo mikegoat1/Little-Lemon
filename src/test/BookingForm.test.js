4d994297e719f26b34faf2ee3a5e7632c955b9ce
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-05-29 19:41:31 -0700

    adding tests

--- src/test/BookingForm.test.js
+++ src/test/BookingForm.test.js
@@ -1,2 +1,11 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import BookingForm from "../components/BookingForm"
+import BookingForm from "../components/BookingForm";
+
+test("Render the BookingForm FormLabels", () => {
+    render(<BookingForm />);
+
+
+    const formLabelElementF = screen.getByText("FIRST NAME");
+
+    expect(formLabelElementF).toBeInTheDocument();
+});
