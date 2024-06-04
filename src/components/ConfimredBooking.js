99881613f23debe0ff93c599d1b70601f0eb205b
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-06-04 10:52:25 -0700

    adding component to ConfirmedBooking

--- src/components/ConfimredBooking.js
+++ src/components/ConfimredBooking.js
@@ -1,2 +1,42 @@
 import React from "react";
-import { Box, Heading, VStack } from "@chakra-ui/react";
+import { useLocation } from "react-router-dom";
+import { Box, Heading, VStack, Text } from "@chakra-ui/react";
+
+
+
+
+const ConfirmedBooking = () => {
+    const location = useLocation(); 
+    const {email, firstName, guestNumber, lastName, occasion, phoneNumber,time} = location.state || {}; 
+    return (
+        <>
+            <Box >
+                <Heading> Your Reservation is Confirmed</Heading>
+                <VStack>
+                    <Text>
+                        Name: {firstName} {lastName}
+                    </Text>
+                    <Text>
+                        Email: {email}
+                    </Text>
+                    <Text>
+                        Phone: {phoneNumber}
+                    </Text>
+                    <Text>
+                        Guest Number: {guestNumber}
+                    </Text>
+                    <Text>
+                        Reservation Time: {time}
+                    </Text>
+                    <Text>
+                        Occasion: {occasion}
+                    </Text>
+
+                </VStack>
+            </Box>
+
+        </>
+    )
+};
+
+export default ConfirmedBooking;
