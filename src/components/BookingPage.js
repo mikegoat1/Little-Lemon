14a1c253bde6817e3898a1fb824bcdb49623610b
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-06-04 12:10:38 -0700

    adding finishing touches

--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useReducer } from "react";
 import FormSubmit from "./BookingForm";
-import { fetchAPI} from "../hooks/api"
+import { fetchAPI } from "../hooks/api"
 
 
 const initializeTimes = () => {
@@ -21,6 +21,8 @@ const updateTimes = (state, action) => {
 };
 
 const BookingPage = () => {
+
+
     const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
 
     useEffect(() => {
--- src/components/ConfimredBooking.js
+++ src/components/ConfimredBooking.js
@@ -6,13 +6,30 @@ import { Box, Heading, VStack, Text } from "@chakra-ui/react";
 
 
 const ConfirmedBooking = () => {
-    const location = useLocation(); 
-    const {email, firstName, guestNumber, lastName, occasion, phoneNumber,time} = location.state || {}; 
+    const location = useLocation();
+    const { email, firstName, guestNumber, lastName, occasion, phoneNumber, time } = location.state || {};
     return (
         <>
-            <Box >
-                <Heading> Your Reservation is Confirmed</Heading>
-                <VStack>
+            <Box w={"100%"} backgroundColor={"#F4CE14"} pb={"2%"} borderBottom={"2px"}>
+                <Heading
+                    textAlign={"center"}
+                    p={"3%"}
+                    fontFamily="Markazi Text"
+                    fontSize={55}
+                    borderTop={"2px"}
+                > Your Reservation is Confirmed</Heading>
+                <VStack
+                    backgroundColor={"white"}
+                    w={"fit-content"}
+                    m={"0 auto"}
+                    p={"3%"}
+                    // marginBottom={"10%"}
+                    fontFamily="karla"
+                    fontSize="20px"
+                    fontWeight="bold"
+                    border={"3px solid black"}
+                    borderRadius={"5%"}
+                    >
                     <Text>
                         Name: {firstName} {lastName}
                     </Text>
