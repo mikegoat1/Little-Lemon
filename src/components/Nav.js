c5836092834e73e7a1263a0ee7853675f11c67b8
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-06-05 09:35:57 -0700

    adjusting the semantics and aria for Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,65 +1,50 @@
 import React from "react";
 import { Image, Box } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import headerLogo from "../icons_assets/LogoHeader.png"
+import headerLogo from "../icons_assets/LogoHeader.png";
 
-const links = [{
-    name: "Home",
-    url: "/"
-},
-{
-    name: "About",
-    url: "/about"
-},
-{
-    name: "Menu",
-    url: "/menu"
-},
-{
-    name: "Reservations",
-    url: "/reservations"
-},
-{
-    name: "Order Online",
-    url: "/order_online"
-},
-{
-    name: "Login",
-    url: "/login"
-}
-]
+const links = [
+    { name: "Home", url: "/" },
+    { name: "About", url: "/about" },
+    { name: "Menu", url: "/menu" },
+    { name: "Reservations", url: "/reservations" },
+    { name: "Order Online", url: "/order_online" },
+    { name: "Login", url: "/login" }
+];
 
 const Nav = () => {
     return (
-        <>
-            <Box maxWidth="1280px" margin="0 20% 0 2%">
-
-                <Box
-                    display="flex"
-                    justifyContent="space-between"
-                    px={32}
-                    py={8}
-                    alignItems="center"
-                    fontFamily="Karla"
-                    fontSize={20}
-                >
-                    <nav>
-                        <Image src={headerLogo} alt="Little Lemon logo" width={260}></Image>
-                    </nav>
-                    <ul style={{ listStyleType: 'none', padding: 0, margin: 0, display: 'flex' }}>
-                        {links.map((data, index) => {
-                            return (
-                                <li key={index} style={{ marginRight: "20px" }}>
-                                    <Link to={data.url} style={{ textDecoration: "none", color: "black", fontWeight: "bold" }}>{data.name}</Link>
-                                </li>
-                            )
-                        })}
-                    </ul>
-
-                </Box>
+        <Box maxWidth="1280px" margin="0 20% 0 2%">
+            <Box
+                display="flex"
+                justifyContent="space-between"
+                px={32}
+                py={8}
+                alignItems="center"
+                fontFamily="Karla"
+                fontSize={20}
+                as="nav"
+                aria-label="Main navigation"
+            >
+                <Link to="/">
+                    <Image src={headerLogo} alt="Little Lemon logo" width={260} />
+                </Link>
+                <ul style={{ listStyleType: 'none', padding: 0, margin: 0, display: 'flex' }}>
+                    {links.map((data, index) => (
+                        <li key={index} style={{ marginRight: "20px" }}>
+                            <Link
+                                to={data.url}
+                                style={{ textDecoration: "none", color: "black", fontWeight: "bold" }}
+                                aria-label={data.name}
+                            >
+                                {data.name}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
             </Box>
-        </>
-    )
-}
+        </Box>
+    );
+};
 
 export default Nav;
